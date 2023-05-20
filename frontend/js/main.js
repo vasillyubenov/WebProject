4b5f86bfa7c42427be1c3b7fd50f0b12f10cd1ba
Vasil Lyubenov <vasillyubenov01@gmail.com>
2023-05-20 16:51:13 +0300

    Stylized form field and added some functionality and loader

---
+++ frontend/js/main.js
@@ -0,0 +1,32 @@
+// IIFE, because we are cool :)
+(() => {
+    const formSubmiter = document.getElementById("form-sumbitter");
+    const referatInput = document.getElementById("referat-input");
+    const audioInput = document.getElementById("audio-input");
+    const configInput = document.getElementById("config-input");
+    const loader = document.getElementById('gray-background-loader');
+    console.log(referatInput.files);
+
+    formSubmiter.addEventListener('click', function(event) {
+        // Prevent the default behavior
+        event.preventDefault();
+        if (referatInput.files.length < 1){
+            alert("Some of the input fields have unselected files");
+            ShowLoader();
+            return;
+        }
+       
+      });
+
+      function ShowLoader() {
+        if (loader) {
+            loader.style.display = 'flex';
+        }
+      }
+
+      function HideLoader() {
+        if (loader) {
+            loader.style.display = 'none';
+        }
+      }
+})();
