ce8fc1e570e3f83fa5f356f324cf45fd4ab3d116
Alex Kolachev <alex.kolachev@trading212.com>
2023-05-20 17:41:46 +0300

    Update backend to return plain text

--- backend/upload.php
+++ backend/upload.php
@@ -1,31 +1,29 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+use Html2Text\Html2Text;
+
 $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["htmlFile"]["name"]);
+$target_file = $target_dir . basename("referat.html");
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-// Check file size
-if ($_FILES["htmlFile"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
-
-// Allow certain file formats
-// if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-// && $imageFileType != "gif" ) {
-//   echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-//   $uploadOk = 0;
-// }
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+    echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
 } else {
-  if (move_uploaded_file($_FILES["htmlFile"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["htmlFile"]["name"])). " has been uploaded.";
-  } else {
-    echo "Sorry, there was an error uploading your file.";
-  }
+    if (move_uploaded_file($_FILES["htmlFile"]["tmp_name"], $target_file)) {
+        echo "The file " . htmlspecialchars(basename($_FILES["htmlFile"]["name"])) . " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
 }
+
+
+$htmlFile = 'uploads/referat.html';
+$htmlContent = file_get_contents($htmlFile);
+$html2text = new Html2Text($htmlContent);
+$textContent = $html2text->getText();
+
+echo $textContent;
 ?>
