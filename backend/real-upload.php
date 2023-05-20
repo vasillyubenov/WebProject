4fe441d0be5787dae88d8ac17971f8a2b1b54147
Alex Kolachev <alex.kolachev@trading212.com>
2023-05-20 19:17:58 +0300

    Add real upload

---
+++ backend/real-upload.php
@@ -0,0 +1,30 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$target_dir = "uploads/";
+$curent_datetime = date("Y:m:d_H:i:s");
+
+if (isset($_FILES['htmlFile'])) {
+    $file_form_name = "htmlFile";
+    upload_file($file_form_name, $target_dir, $curent_datetime);
+}
+
+if (isset($_FILES['audioFile'])) {
+    $file_form_name = "audioFile";
+    upload_file($file_form_name, $target_dir, $curent_datetime);
+}
+
+function upload_file($file_form_name, $target_dir, $curent_datetime)
+{
+    $target_file = $target_dir . basename($curent_datetime . $file_form_name);
+    // echo $target_file."\n";
+    // echo $_FILES."\n";
+    // print_r($_FILES[$file_form_name]);
+    if (move_uploaded_file($_FILES[$file_form_name]["tmp_name"], $target_file)) {
+        echo "The file " . basename($_FILES[$file_form_name]["name"]) . " has been uploaded.\n";
+    } else {
+        echo "Sorry, there was an error uploading your " . basename($_FILES[$file_form_name]["name"]) . "\n";
+        return;
+    }
+}
+?>
