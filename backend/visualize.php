53de81e5e85b4c7050f1a6a27d56a924e185d2d2
Alex Kolachev <alex.kolachev@trading212.com>
2023-05-24 17:30:32 +0300

    Add audio sync

--- backend/visualize.php
+++ backend/visualize.php
@@ -4,13 +4,14 @@ use Html2Text\Html2Text;
 
 $time = $_GET["time"];
 $target_dir = "uploads/";
-$audio_file =  $target_dir . $time . basename("audioFile");
+$audio_file = $target_dir . $time . basename("audioFile");
 $html_file = $target_dir . $time . basename("htmlFile");
 ?>
 
 <html lang="en">
 
 <head>
+  <script src='../frontend/js/animation.js'></script>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -44,6 +45,23 @@ $html_file = $target_dir . $time . basename("htmlFile");
     }
   </script>
 
+  <div style="position: absolute;
+    z-index: 2;
+    display: inline-block;
+    padding: 2px 4px;
+    font-family: 'Star Jedi', sans-serif;
+    font-size: 12px;
+    text-decoration: none;
+    color: #FFD700;
+    border: 1px solid #FFD700;
+    background-color: #000;
+    transition: all 0.3s ease;">
+
+
+    <audio id="audioPlayer" src="../frontend/assets/audio/Star_Wars_Main_Theme.mp3" style="display: none"></audio>
+
+  </div>
+
   <div class="fade"></div>
   <section class="star-wars">
     <div id="crawl">
@@ -51,25 +69,29 @@ $html_file = $target_dir . $time . basename("htmlFile");
         <p>Episode IV</p>
         <h1>A New Hope</h1>
       </div>
-    <?php
-    $chunkSize = 5;
-    
-    $audio = file_get_contents($audio_file);
-    $html = file_get_contents($html_file);
-    
-    $options = array('ignore_comments' => true);
-    $html2text = new Html2Text($html, $options);
-    $textContent = $html2text->getText();
+      <?php
+      $chunkSize = 5;
 
-    $textContent = str_replace('*', "\n", $textContent);
-    $textContent = nl2br($textContent);
-    
-    $words = str_word_count($textContent, 1); // get words as an array
-    $chunks = array_chunk($words, $chunkSize);
-    echo '<p>' . $textContent . '</p>';
-    echo "<script src='../frontend/js/animation.js'></script>";
-    echo "<script>StartAnimation('" . htmlspecialchars($audioFile) . "');</script>";
-    ?>
+      $audio = file_get_contents($audio_file);
+      $html = file_get_contents($html_file);
+
+      $options = array('ignore_comments' => true);
+      $html2text = new Html2Text($html, $options);
+      $textContent = $html2text->getText();
+
+      $textContent = str_replace('*', "\n", $textContent);
+      $textContent = nl2br($textContent);
+
+      $words = str_word_count($textContent, 1); // get words as an array
+      $chunks = array_chunk($words, $chunkSize);
+      echo '<p>' . $textContent . '</p>';
+      echo "<script>StartAnimation();</script>";
+      // echo "<script> 
+      // document.getElementById('playButton').addEventListener('click', function() {
+      //                 StartAnimation();
+      //                      });
+      //                      </script>";
+      ?>
   </section>
 
 </body>
--- frontend/js/animation.js
+++ frontend/js/animation.js
@@ -1,110 +1,110 @@
-class Animation {
-    constructor(data, config) {
-        this.data = data;
-        this.config = config;
-    }
-    // methods to handle animations
+function PlayAudio() {
+  var audio = document.getElementById("audioPlayer");
+  audio.play();
 }
 
-function StartAnimation(audioFilePath, playTime) {
-    var isPaused = true;
-    let topValue = -100;
-    let translateZValue = 0;
-    let incrementValue = 1;
-    const defaultIncrementValue = 1;
-    let crawlAnimationInterval;
-    const updateTime = 10;
-    let fastForwardIntervalId;
-    let fastBackwardIntervalId;
-    let fastingStep = 4;
+function PauseAudio() {
+  var audio = document.getElementById("audioPlayer");
+  audio.pause();
+}
 
-    ToggleAnimation();
-    // PlayMusic(audioFilePath, playTime);
+function SeekForward() {
+  var audio = document.getElementById("audioPlayer");
+  audio.currentTime += 5;
+}
 
-    document.addEventListener('keydown', function(event) {
-        if (event.code === 'Space') {
-            ToggleAnimation();
-        }
+function SeekBackwards() {
+  var audio = document.getElementById("audioPlayer");
+  audio.currentTime -= 5;
+}
+
+function StartAnimation() {
+  var isPaused = false;
+  let topValue = -100;
+  let translateZValue = 0;
+  let incrementValue = 1;
+  const defaultIncrementValue = 1;
+  let crawlAnimationInterval;
+  const updateTime = 10;
+  let fastForwardIntervalId;
+  let fastBackwardIntervalId;
+  let fastingStep = 4;
+
+  ToggleAnimation();
+
+  document.addEventListener("keydown", function (event) {
+    if (event.code === "Space") {
+      ToggleAnimation();
+    }
+  });
+
+  function ToggleAnimation() {
+    isPaused = !isPaused;
+    if (!isPaused) {
+      StartAnimationLoop();
+      PlayAudio();
+      return;
+    }
+
+    PauseAnimationLoop();
+    PauseAudio();
+  }
+
+  (function ActivateControls() {
+    //Activating fast backward
+    window.addEventListener("keydown", function (event) {
+      if (event.key === "ArrowLeft" && fastBackwardIntervalId == null) {
+        fastBackwardIntervalId = setInterval(FastBackward, 100);
+      }
     });
 
-    function ToggleAnimation() {
-        isPaused = !isPaused;
-        if (!isPaused) {
-            StartAnimationLoop();
-            return;
-        }
+    window.addEventListener("keyup", function (event) {
+      if (event.key === "ArrowLeft" && fastBackwardIntervalId != null) {
+        clearInterval(fastBackwardIntervalId);
+        fastBackwardIntervalId = null;
+        incrementValue = defaultIncrementValue;
+      }
+    });
 
-        PauseAnimationLoop();
+    //Activating fast forward
+    window.addEventListener("keydown", function (event) {
+      if (event.key === "ArrowRight" && fastForwardIntervalId == null) {
+        fastForwardIntervalId = setInterval(FastForward, 100);
+      }
+    });
+
+    window.addEventListener("keyup", function (event) {
+      if (event.key === "ArrowRight" && fastForwardIntervalId != null) {
+        clearInterval(fastForwardIntervalId);
+        fastForwardIntervalId = null;
+        incrementValue = defaultIncrementValue;
+      }
+    });
+  })();
+
+  function FastForward() {
+    incrementValue = fastingStep;
+  }
+
+  function FastBackward() {
+    incrementValue = -fastingStep;
+  }
+
+  function StartAnimationLoop() {
+    crawlAnimationInterval = setInterval(function () {
+      topValue -= incrementValue;
+      translateZValue -= incrementValue * 0.41;
+
+      document.getElementById("crawl").style = `
+                  top: ${topValue}px;
+                  transform: rotateX(25deg) translateZ(${translateZValue}px);
+              `;
+    }, updateTime);
+  }
+
+  function PauseAnimationLoop() {
+    if (crawlAnimationInterval) {
+      clearInterval(crawlAnimationInterval);
     }
-
-    (function ActivateControls() {
-        //Activating fast backward
-        window.addEventListener('keydown', function(event) {
-            if (event.key === "ArrowLeft" && fastBackwardIntervalId == null) {
-                fastBackwardIntervalId = setInterval(FastBackward, 100);
-            }
-        });
-    
-        window.addEventListener('keyup', function(event) {
-            if (event.key === "ArrowLeft" && fastBackwardIntervalId != null) {
-                clearInterval(fastBackwardIntervalId);
-                fastBackwardIntervalId = null;
-                incrementValue = defaultIncrementValue;
-            }
-        });
-
-        //Activating fast forward
-        window.addEventListener('keydown', function(event) {
-            if (event.key === "ArrowRight" && fastForwardIntervalId == null) {
-                fastForwardIntervalId = setInterval(FastForward, 100);
-            }
-        });
-    
-        window.addEventListener('keyup', function(event) {
-            if (event.key === "ArrowRight" && fastForwardIntervalId != null) {
-                clearInterval(fastForwardIntervalId);
-                fastForwardIntervalId = null;
-                incrementValue = defaultIncrementValue;
-            }
-        });
-    })();
-
-    function FastForward() {
-        incrementValue = fastingStep;
-    }
-
-    function FastBackward() {
-        incrementValue = -fastingStep;
-    }
-
-    function PlayMusic(filename, playTime) {
-        console.log(filename);
-        const defaultAudioPath = "../assets/audio/Star_Wars_Main_Theme.mp3";
-        
-        audio = new Audio(filename !== "" ? filename : defaultAudioPath);
-        audio.play();
-
-        if (playTime) {
-            audio.currentTime = playTime;
-        }
-    }
-
-    function StartAnimationLoop() {
-        crawlAnimationInterval = setInterval(function(){
-            topValue -= incrementValue;
-            translateZValue -= incrementValue * 0.41;
-            
-            document.getElementById('crawl').style = `
-                top: ${topValue}px;
-                transform: rotateX(25deg) translateZ(${translateZValue}px);
-            `;
-        }, updateTime);
-    }
-
-    function PauseAnimationLoop() {
-        if (crawlAnimationInterval) {
-            clearInterval(crawlAnimationInterval);
-        }
-    }
-};
-
+  }
+}
